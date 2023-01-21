17c8ebaaea0f554aa24a4a3b5b33e33cabaabfc6
Oleksii Proshchenko <80070761+MasterpieceElbow@users.noreply.github.com>
2023-01-21 16:47:32 +0200

    added .env support

--- config.py
+++ config.py
@@ -1,1 +1,13 @@
+import os 
+
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+SQLALCHEMY_DATABASE_URL = os.getenv("SQLALCHEMY_DATABASE_URL")
+DOMAIN_ADDRESS = "https://97y3sv.deta.dev/"
 LINK_NAME_LENGHT = 6
+MIN_DAYS_TO_EXPIRE = 1
+MAX_DAYS_TO_EXPIRE = 365
+DEFAULT_DAYS_TO_EXPIRE = 90
